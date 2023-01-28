31a535b19cc092ce458a966fae7660508c6703c1
RIKO-99 <princericko6@gmal.com>
2023-01-28 09:01:11 -0800

    doubly linked

--- sort.h
+++ sort.h
@@ -9,4 +9,20 @@
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+/*Struct data type*/
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
 #endif /*_SORT_H_*/
